52004aaedb6f50456dd7e1db0b7f45abbb0a4325
Jhulai91 <100226190+Jhulai91@users.noreply.github.com>
2026-03-27 22:41:54 -0500

    cambio docente
    docet entity

--- EduControl/src/main/java/com/educontrol/entity/Docente.java
+++ EduControl/src/main/java/com/educontrol/entity/Docente.java
@@ -1,8 +1,10 @@
 package com.educontrol.entity;
 
 import jakarta.annotation.Nonnull;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,44 +22,42 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Docente {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "id_docente")
-	private Integer idDocente;
-	
-	@OneToOne
-	@JoinColumn(name="id_usuario")
-	private Usuario usuario;
-	
-	@ManyToOne
-	@JoinColumn(name="id_especializacion", nullable = false)
-	private Especializacion especializacion;
-	
-	@NotNull
-	@Column(name = "docente_cedula")
-    private String docenteCedula;
-	
-	@NotNull
-	@Column(name = "docente_nombres")
-    private String docenteNombres;
-	
-	@NotNull
-	@Column(name = "docente_apellidos")
-    private String docenteApellidos;
-	
-	@NotNull
-	@Column(name = "docente_telefono")
-    private String docenteTelefono;
-	
-	@NotNull
-	@Column(name = "docente_email")
-    private String docenteEmail;
-	
-	@NotNull
-	@Column(name = "docente_estado")
-    private String docenteEstado;
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id_docente")
+    private Integer idDocente;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="id_usuario", nullable = false, unique = true)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id_especializacion", nullable = false)
+    private Especializacion especializacion;
+
+    @NotNull
+    @Column(name = "docente_cedula", nullable = false, unique = true)
+    private String docenteCedula;
+
+    @NotNull
+    @Column(name = "docente_nombres", nullable = false)
+    private String docenteNombres;
+
+    @NotNull
+    @Column(name = "docente_apellidos", nullable = false)
+    private String docenteApellidos;
+
+    @NotNull
+    @Column(name = "docente_telefono", nullable = false)
+    private String docenteTelefono;
+
+    @NotNull
+    @Column(name = "docente_email", nullable = false)
+    private String docenteEmail;
+
+    @NotNull
+    @Column(name = "docente_estado", nullable = false)
+    private String docenteEstado;
 }
